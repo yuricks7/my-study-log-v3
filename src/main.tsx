c1121f325e1571ab11ad7be78e5605b0e312e15a
20250824_yuricks7 <yuricks7+mousePC@dekoboko-hack.biz>
2026-07-11 12:24:43 +0900

    removed Unnecessary Import Statements.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from '@/components/ui/provider';
 
-import './index.css'
-
-import App from './App.tsx'
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
